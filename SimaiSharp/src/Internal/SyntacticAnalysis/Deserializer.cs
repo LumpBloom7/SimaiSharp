2b2630930eaf9205d8d358490d0b93e1f083f68f
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-12-14 22:32:31 +0100

    Whitespace fixes

--- SimaiSharp/src/Internal/SyntacticAnalysis/Deserializer.cs
+++ SimaiSharp/src/Internal/SyntacticAnalysis/Deserializer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using SimaiSharp.Internal.Errors;
 using SimaiSharp.Internal.LexicalAnalysis;
--- SimaiSharp/src/Internal/SyntacticAnalysis/States/TempoReader.cs
+++ SimaiSharp/src/Internal/SyntacticAnalysis/States/TempoReader.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Globalization;
 using System.Runtime.CompilerServices;
-using System.Xml.XPath;
 using SimaiSharp.Internal.Errors;
 using SimaiSharp.Internal.LexicalAnalysis;
 
